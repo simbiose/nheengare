82528aff7bf3667317cc9633428ad05f906bed46
Ademar Alves de Oliveira <ademar111190@gmail.com>
2013-05-13 00:19:39 -0300

    using the Levenshtein Distance to search strings

--- mobile/android/src/simbio/se/nheengare/models/AbstractModel.java
+++ mobile/android/src/simbio/se/nheengare/models/AbstractModel.java
@@ -79,11 +79,31 @@ public class AbstractModel implements Comparable<AbstractModel>, Serializable, I
 	}
 
 	public void setCriteriaWeight(String compareToCriteria) {
-		if (compareToCriteria.contains(criteria)) {
-			criteriaWeight = CRITERIA_MAX - compareToCriteria.length();
-		} else {
-			criteriaWeight = CRITERIA_MIN;
-		}
+		if (criteria.length() == 0)
+			criteriaWeight = CRITERIA_MAX;
+		else
+			criteriaWeight = CRITERIA_MAX - computeLevenshteinDistance(criteria, compareToCriteria);
+	}
+
+	private static int minimum(int a, int b, int c) {
+		return Math.min(Math.min(a, b), c);
+	}
+
+	/**
+	 * Levenshtein Distance
+	 * http://en.wikibooks.org/wiki/Algorithm_Implementation
+	 * /Strings/Levenshtein_distance#Java
+	 */
+	public static int computeLevenshteinDistance(CharSequence str1, CharSequence str2) {
+		int[][] distance = new int[str1.length() + 1][str2.length() + 1];
+		for (int i = 0; i <= str1.length(); i++)
+			distance[i][0] = i;
+		for (int j = 1; j <= str2.length(); j++)
+			distance[0][j] = j;
+		for (int i = 1; i <= str1.length(); i++)
+			for (int j = 1; j <= str2.length(); j++)
+				distance[i][j] = minimum(distance[i - 1][j] + 1, distance[i][j - 1] + 1, distance[i - 1][j - 1] + ((str1.charAt(i - 1) == str2.charAt(j - 1)) ? 0 : 1));
+		return distance[str1.length()][str2.length()];
 	}
 
 	public float getCriteriaWeight() {
